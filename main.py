d3e176f9284ddff8091f93901eee80b40789dbdf
tommy <tommyhuynh64@gmail.com>
2020-10-17 15:16:34 -0400

    navbar

--- main.py
+++ main.py
@@ -2,9 +2,26 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
+def login():
+    return render_template("login.html")
+
+@app.route("/create")
+def create():
+    return render_template("create.html")
+
+@app.route("/leaderboard")
 def leaderboard():
     return render_template("leaderboard.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/donate")
+def donate():
+    return render_template("donate.html")
     
 if __name__ == "__main__":
     app.run(debug=True)
